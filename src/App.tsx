4e156d781e9ed6e6e9e9f589e5adc5ab43ff15bd
prolewall <h.kowalik98@gmail.com>
2023-07-31 22:09:39 +0200

    add theme switching functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import './App.scss';
 import Calculator from './components/Calculator';
 import ThemeToggle from './components/ThemeToggle';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [themeClass, setThemeClass] = useState("theme-1");
+
   return (
-    <div className="App theme-1">
+    <div className={`App ${themeClass}`}>
       <div className="App__content">
         <div className="App__settings">
           <p className="App__name">calc</p>
-          <ThemeToggle />
+          <ThemeToggle themeChangeCallback={setThemeClass}/>
         </div>
         <Calculator />
       </div>
--- src/components/ThemeToggle/themeToggle.tsx
+++ src/components/ThemeToggle/themeToggle.tsx
@@ -1,14 +1,21 @@
 import "./themeToggle.scss";
 
-const ThemeToggle:React.FC = () => {
+interface ThemeToggleProps {
+    themeChangeCallback: (themeClass: string) => void;
+}
+
+const ThemeToggle:React.FC<ThemeToggleProps> = ({themeChangeCallback}) => {
+
+    
+
     return (
         <div className="ThemeToggle">
             <p className="ThemeToggle__label">THEME</p>
-            <div className="ThemeToggle__toggle">
-                <label htmlFor="theme-1"><input type="radio" id="theme-1" name="theme" /><span></span></label>
-                <label htmlFor="theme-2"><input type="radio" id="theme-2" name="theme" /><span></span></label>
-                <label htmlFor="theme-3"><input type="radio" id="theme-3" name="theme" /><span></span></label>
-            </div>
+            <div className="ThemeToggle__toggle" onChange={(event: any) => themeChangeCallback(event.target.value)}>
+                <label htmlFor="theme-1"><input type="radio" id="theme-1" name="theme" value="theme-1" defaultChecked/><span></span></label>
+                <label htmlFor="theme-2"><input type="radio" id="theme-2" name="theme" value="theme-2"/><span></span></label>
+                <label htmlFor="theme-3"><input type="radio" id="theme-3" name="theme" value="theme-3"/><span></span></label>
+            </div >
         </div>
     );
 }
